091b76b2fa693da14c85b53a6d257f104a1dfa4d
sanabuk <42888795+sanabuk@users.noreply.github.com>
2019-12-06 15:54:12 +0100

    Update larafast.php

--- config/larafast.php
+++ config/larafast.php
@@ -6,9 +6,13 @@ return [
 	'route' => 'lara',
 	'models' => [
 		'driver' => 'sanabuk\\driver\\models\\Driver',
-		'vehicle' => 'sanabuk\\driver\\models\\Vehicle'
+		'vehicle' => 'sanabuk\\driver\\models\\Vehicle',
+		'historic' => 'sanabuk\\driver\\models\\HistoryDriverVehicle',
+		'user' => 'App\\User'
 	],
     'driver'   => ['id'],
+    'drivers'  => ['id','user_id'],
     'vehicle'  => ['id', 'driver_id'],
     'historic' => ['driver_id', 'vehicle_id'],
+    'user' => ['id']
 ];
